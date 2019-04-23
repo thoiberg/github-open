56ebcd64b2a2bc53c2a641ad7fb0bcf48cdcc714
Tim Hoiberg <tim.hoiberg@envato.com>
2019-04-23 12:43:19 +1000

    Adding regex statement to pull the repo name from the git remote

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,10 @@
 // combine that with the repo name to get a link to the file
 // `open` it
 
+extern crate regex;
+
 use std::process::Command;
+use regex::Regex;
 
 // TODO: Return a Result instead
 // Check if the status is successful then either return Ok with the String or Err
@@ -16,13 +19,23 @@ fn get_git_remotes() -> String {
     println!("output: {:?}", unwrapped_output);
     println!("status: {}", unwrapped_output.status);
     println!("was successful?: {}", unwrapped_output.status.success());
-    println!("stdout: {:?}", unwrapped_output.stdout);
 
     return String::from(String::from_utf8_lossy(&unwrapped_output.stdout));
 }
 
 fn main() {
     let git_remotes = get_git_remotes();
-
     println!("git remotes: {}", git_remotes);
+
+    // origin	git@github.com:thoiberg/github-open.git (fetch)
+    // origin	git@github.com:thoiberg/github-open.git (push)
+    let re = Regex::new(r".+:(.+)\.git").unwrap();
+    let mut captures: Vec<String> = Vec::new();
+    for cap in re.captures_iter(&git_remotes[..]) {
+        captures.push(String::from(&cap[1]));
+        println!("the repo name is: {}", &cap[1]);
+    }
+
+    println!("The captured repo names are: {:?}", captures);
+
 }
