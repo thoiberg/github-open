b087594547a7d1227ab350b91db099498f33f89c
Tim Hoiberg <tim.hoiberg@envato.com>
2019-05-01 21:07:09 +1000

    Moving the git access code into the library
    Moving the git code into the lib.rs file so I can also access it as an
    API. In preparation for turning this into a crate

--- src/main.rs
+++ src/main.rs
@@ -6,47 +6,10 @@
 // combine that with the repo name to get a link to the file
 // `open` it
 
-extern crate regex;
-
-use regex::Regex;
 use std::env;
 use std::path::Path;
-use std::process::Command;
 
-// TODO: Return a Result instead
-// Check if the status is successful then either return Ok with the String or Err
-fn get_git_remotes() -> String {
-    let output = Command::new("git").arg("remote").arg("-v").output();
-    let unwrapped_output = output.unwrap();
-
-    return String::from(String::from_utf8_lossy(&unwrapped_output.stdout));
-}
-
-// TODO: Return a Result instead
-// Check if the status is successful then either return Ok with the String or Err
-fn get_git_repo_from_remotes(git_remotes: String) -> Vec<String> {
-    // origin	git@github.com:thoiberg/github-open.git (fetch)
-    // origin	git@github.com:thoiberg/github-open.git (push)
-
-    let re = Regex::new(r".+:(.+)\.git").unwrap();
-    let mut captures: Vec<String> = Vec::new();
-    for cap in re.captures_iter(&git_remotes[..]) {
-        captures.push(String::from(&cap[1]));
-    }
-
-    return captures;
-}
-
-fn get_git_root() -> String {
-    // git rev-parse --show-toplevel
-    let output = Command::new("git")
-        .arg("rev-parse")
-        .arg("--show-toplevel")
-        .output();
-    let unwrapped_output = output.unwrap();
-
-    return String::from(String::from_utf8_lossy(&unwrapped_output.stdout));
-}
+use github_open::*;
 
 fn main() {
     // TODO: add graceful handling to deal with the wrong amount/type of args being passed in
